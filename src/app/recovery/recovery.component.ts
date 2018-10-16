38e13dece4448621f01abf2c8a13939ff7f60015
SmailMeziani <smeziani@bk-consulting.com>
2018-10-16 10:59:54 +0200

    :sparkles: [Recovery] Introducing backbody.png and flip button

--- src/app/recovery/recovery.component.ts
+++ src/app/recovery/recovery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recovery',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoveryComponent implements OnInit {
 
+	bodyImg: string = 'frontbody'
+
   constructor() { }
 
   ngOnInit() {
   }
 
+	onFlipClick() {
+		if (this.bodyImg == 'frontbody') {
+			this.bodyImg = 'backbody'
+		} else {
+			this.bodyImg = 'frontbody'
+		}
+	}
 }
